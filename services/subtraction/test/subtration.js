e965cc8609c975fa7e56ed9fd8f21822cddb8a20
praveen kumar kantubotu <kantubotupraveenkumar@gmail.com>
2025-02-28 23:11:15 +0530

    try experssion

--- services/subtraction/test/subtration.js
+++ services/subtraction/test/subtration.js
@@ -1,6 +1,6 @@
 const subtraction = require("../src/actions/subtraction");
 
-var assert = require("assert");
+const assert = require("assert");
 describe("subtraction", function() {
   it("1 - 1 should be 0 ", function() {
     assert.equal(subtraction(1, 1), 0);
